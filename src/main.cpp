71e2dd4cd4bf56c926ae431b84e77f5c7d6a18a2
Luke Faraone <lfaraone@humbughq.com>
2013-06-05 15:40:50 -0400

    Declare an application name and version.

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,14 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setApplicationName("Humbug Desktop");
+    a.setApplicationVersion("0.1");
+
     HumbugWindow w;
     if (argc == 3 && QString(argv[1]) == QString("--site")) {
         w.setUrl(QUrl(argv[2]));
     }
+
     w.show();
 
     return a.exec();
