c1f29ea1793b05de19e7e8e042b10d923e4aea60
Leo Franchi <leo@humbughq.com>
2013-07-01 11:49:18 -0400

    Fix OS X make_package clone URL to gitolite URL

--- admin/mac/make_package.sh
+++ admin/mac/make_package.sh
@@ -10,7 +10,7 @@ fi
 
 cd /tmp
 rm -rf humbug-desktop
-git clone humbug@git.humbughq.com:/srv/git/humbug-desktop.git
+git clone git@git.humbughq.com:humbug-desktop.git
 pushd humbug-desktop
 mkdir build
 cd build
