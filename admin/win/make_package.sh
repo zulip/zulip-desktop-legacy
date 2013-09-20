25135d9a222e1c5e194eb6d42fab4d68a60215b0
Leo Franchi <leo@humbughq.com>
2013-09-20 17:00:09 -0400

    Sign windows binary and installer from linux during packaging

--- admin/win/make_package.sh
+++ admin/win/make_package.sh
@@ -17,8 +17,16 @@ popd
 mkdir build
 cd build
 cmake -DCMAKE_TOOLCHAIN_FILE=../admin/win/Toolchain-mingw32-openSUSE.cmake -DCMAKE_BUILD_TYPE=Release ..
+
 make -j3
+
+signcode -spc ~/real_windows_key.spc -v ~/real_windows_key.pvk -a sha1 -$ commercial -n Zulip -i https://zulip.com \
+  -t http://timestamp.verisign.com/scripts/timstamp.dll -tr 10 zulip.exe < ~/windows_signing_passphrase.private
+
 make package
+
+signcode -spc ~/real_windows_key.spc -v ~/real_windows_key.pvk -a sha1 -$ commercial -n Zulip -i https://zulip.com \
+  -t http://timestamp.verisign.com/scripts/timstamp.dll -tr 10 zulip-$1.exe < ~/windows_signing_passphrase.private
 
 mv zulip-$1.exe ~/packages/
 
