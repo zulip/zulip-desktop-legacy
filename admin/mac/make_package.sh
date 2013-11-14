0c1ea05fcee70ea5dcef7d210450f7d1c25711f6
Leo Franchi <leo@zulip.com>
2013-11-14 08:30:26 -0500

    Add SSO build support for make_package mac/win

--- admin/mac/make_package.sh
+++ admin/mac/make_package.sh
@@ -2,10 +2,19 @@
 #
 # Builds a zulip desktop app package for OS X
 
-if [ -z $1 ]
+if [ -z "$1" ]
 then
 	echo "Please pass the version to package"
 	exit 1
+fi
+
+if [ -n "$2" ]
+then
+	echo "Starting SSO build"
+        ENABLE_SSO=1
+        SSO_CMD="-DSSO_BUILD=ON"
+else
+	echo "Starting non-SSO build"
 fi
 
 cd /tmp
@@ -14,15 +23,27 @@ git clone git@git.zulip.net:eng/zulip-desktop.git
 pushd zulip-desktop
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release "$SSO_CMD"  ..
 make -j3
 ../admin/mac/build-release-osx.sh "$@"
 
-mv Zulip-$1.dmg ~/packages/
-mv Zulip-$1.tar.bz2 ~/packages/sparkle
-mv signature.raw ~/packages/sparkle/Zulip-$1.tar.bz2.sig
+zulip_bin="Zulip-$1.dmg"
+zulip_sparkle_bin="Zulip-$1.tar.bz2"
+if [ -n "$ENABLE_SSO" ]
+then
+	echo "Making SSO packages"
+        mv "$zulip_bin" "Zulip-$1-sso.dmg"
+        zulip_bin="Zulip-$1-sso.dmg"
+
+        mv "$zulip_sparkle_bin" "Zulip-$1-sso.tar.bz2"
+        zulip_sparkle_bin="Zulip-$1-sso.tar.bz2"
+fi
+
+mv "$zulip_bin" ~/packages/
+mv "$zulip_sparkle_bin" ~/packages/sparkle
+mv signature.raw "~/packages/sparkle/$zulip_sparkle_bin.sig"
 
 popd
 rm -rf zulip-desktop/
 
-echo "Generated DMG Zulip-$1.dmg in ~/packages!"
+echo "Generated DMG $zulip_sparkle and $zulip_sparkle_bin in ~/packages!"
--- admin/win/make_package.sh
+++ admin/win/make_package.sh
@@ -2,10 +2,17 @@
 #
 # Builds a zulip desktop app package for win32
 
-if [ -z $1 ]
+if [ -z "$1" ]
 then
 	echo "Please pass the version to package"
 	exit 1
+fi
+
+if [ -n "$2" ]
+then
+	echo "Building SSO package"
+	ENABLE_SSO=true
+        SSO_CMD="-DSSO_BUILD=ON"
 fi
 
 cd /tmp
@@ -16,7 +23,7 @@ pushd admin/win
 popd
 mkdir build
 cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=../admin/win/Toolchain-mingw32-openSUSE.cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_TOOLCHAIN_FILE=../admin/win/Toolchain-mingw32-openSUSE.cmake -DCMAKE_BUILD_TYPE=Release "$SSO_CMD" ..
 
 make -j3
 
@@ -25,12 +32,19 @@ signcode -spc ~/real_windows_key.spc -v ~/real_windows_key.pvk -a sha1 -$ commer
 
 make package
 
-signcode -spc ~/real_windows_key.spc -v ~/real_windows_key.pvk -a sha1 -$ commercial -n Zulip -i https://zulip.com \
-  -t http://timestamp.verisign.com/scripts/timstamp.dll -tr 10 zulip-$1.exe < ~/windows_signing_passphrase.private
+zulip_bin="zulip-$1.exe"
+if [ -n "$ENABLE_SSO" ]
+then
+        mv "$zulip_bin" "zulip-$1-sso.exe"
+        zulip_bin="zulip-$1-sso.exe"
+fi
 
-mv zulip-$1.exe ~/packages/
+signcode -spc ~/real_windows_key.spc -v ~/real_windows_key.pvk -a sha1 -$ commercial -n Zulip -i https://zulip.com \
+  -t http://timestamp.verisign.com/scripts/timstamp.dll -tr 10 "$zulip_bin" < ~/windows_signing_passphrase.private
+
+mv "$zulip_bin" ~/packages/
 
 popd
 rm -rf zulip-desktop/
 
-echo "Generated zulip-$1.exe installer in ~/packages!"
+echo "Generated $zulip_bin installer in ~/packages!"
