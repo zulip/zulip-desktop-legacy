72de223be956e5ac4f1e45fe28e2ea1954c9dd09
Leo Franchi <leo@zulip.com>
2013-11-19 18:00:26 -0500

    Load extra JS from a file instead of hardcoding it into .mm files

--- src/webview/HWebView_mac.mm
+++ src/webview/HWebView_mac.mm
@@ -487,38 +487,13 @@ public:
 - (void)webView:(WebView *)sender didClearWindowObject:(WebScriptObject *)windowObject forFrame:(WebFrame *)frame {
     [windowObject setValue:self forKey:@"bridge"];
 
-    // The built-in WebKit sendAsBinary crashes when we try to use it with a large
-    // payload---specifically, when pasting large images. The traceback is deep in
-    // array handling code in JSC.
-    //
-    // To avoid the crash, we replace the native sendAsBinary with the suggested polyfill
-    // using typed arrays, from https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest#sendAsBinary()
-    [sender stringByEvaluatingJavaScriptFromString:
-     @"XMLHttpRequest.prototype.sendAsBinary = function (sData) { "
-             "var nBytes = sData.length, ui8Data = new Uint8Array(nBytes); "
-             "for (var nIdx = 0; nIdx < nBytes; nIdx++) { "
-                 "ui8Data[nIdx] = sData.charCodeAt(nIdx) & 0xff; "
-             "} "
-             "this.send(ui8Data.buffer); "
-           "}"
-     ];
-
-    // Hook into websocket creation to swizzle the system cookies
-    [sender stringByEvaluatingJavaScriptFromString:
-     @"document.addEventListener('DOMContentLoaded', function(event) {"
-     "    $(document).on('websocket_preopen.zulip', function (event) {"
-     "        if (typeof window.bridge !== 'undefined' &&"
-     "            typeof window.bridge.websocketPreOpen !== 'undefined') {"
-     "            window.bridge.websocketPreOpen();"
-     "        }"
-     "    });"
-     "    $(document).on('websocket_postopen.zulip', function (event) {"
-     "        if (typeof window.bridge !== 'undefined' &&"
-     "            typeof window.bridge.websocketPostOpen !== 'undefined') {"
-     "            window.bridge.websocketPostOpen();"
-     "        }"
-     "    });"
-     "});"];
+    QFile extraJS(":/mac/JSExtras.js");
+    if (!extraJS.open(QIODevice::ReadOnly)) {
+        qWarning() << "Failed to read from :/mac/JSExtras.js";
+    } else {
+        NSString *contents = fromQString(QString::fromUtf8(extraJS.readAll()));
+        [sender stringByEvaluatingJavaScriptFromString:contents];
+    }
 }
 
 // Grab console.log output to our own logfile
