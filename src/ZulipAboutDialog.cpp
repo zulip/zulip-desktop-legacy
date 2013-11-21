16897e940d6d7573a334b2284cddcfc0d176fcf5
Leo Franchi <leo@zulip.com>
2013-11-21 11:14:48 -0500

    Show appropriate license information in About dialog for SSO builds

--- src/ZulipAboutDialog.cpp
+++ src/ZulipAboutDialog.cpp
@@ -17,7 +17,15 @@ ZulipAboutDialog::ZulipAboutDialog(QWidget *parent) :
     } else {
         QString contents = QString::fromUtf8(html.readAll());
         // Add version string
-        contents = contents.arg(ZULIP_VERSION_STRING);
+
+        QString license;
+#ifdef SSO_BUILD
+        license = "the Zulip Enterprise License Agreement";
+#else
+        license = "the license described in the <a href=\"https://zulip.com/terms\">Zulip Terms of Service</a>";
+#endif
+        contents = contents.replace("{{ZULIP_VERSION_STRING}}", ZULIP_VERSION_STRING);
+        contents = contents.replace("{{ZULIP_LICENSE_AGREEMENT}}", license);
         m_ui->webView->loadHTML(contents);
     }
 }
