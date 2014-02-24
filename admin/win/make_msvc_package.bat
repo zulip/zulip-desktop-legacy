8baf3ad7ee885c8d79849ee210092958c6d57ff4
Zulip Windows <windows-devs@zulip.com>
2014-02-24 13:57:45 -0500

    Add beginning of windows build script

---
+++ admin/win/make_msvc_package.bat
@@ -0,0 +1,52 @@
+@echo off
+
+:: Builds a Zulip Desktop release with MSVC
+
+
+REM set SSO_BUILD=%2
+
+if "%1" == "" (
+    echo No version specified!
+    exit /b
+) else (
+    echo Building Version %1
+)
+
+
+echo Loading MSVC Environment
+call c:\Qt\qt_env.bat
+
+pushd %TMP%
+
+if exist zulip-desktop\ (
+    rmdir zulip-desktop /s /q
+)
+
+git clone git@git.zulip.net:eng/zulip-desktop.git
+pushd zulip-desktop
+git checkout leo-windows-proper-fixes
+mkdir build
+pushd build
+
+echo Ready to build
+
+cmake -DBUILD_WITH_QT5=On -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE=Release ..
+jom
+
+SignTool sign /f C:\Users\zulip\signkey.pfx /p berainyacliplachwayacome /t http://timestamp.verisign.com/scripts/timstamp.dll /d "Zulip" /du "https://zulip.com" zulip.exe
+
+echo Building NSIS package
+nmake package
+
+SignTool sign /f C:\Users\zulip\signkey.pfx /p berainyacliplachwayacome /t http://timestamp.verisign.com/scripts/timstamp.dll /d "Zulip" /du "https://zulip.com" "zulip-%1.exe"
+
+mv "zulip-%1.exe" C:\Users\Zulip\Packages
+
+popd
+popd
+
+rmdir zulip-desktop /s /q
+
+popd
+
+echo Generated zulip-%1.exe in C:\Users\Zulip\Packages!
